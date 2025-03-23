5236f6bfcaf6a67b6395a23c7f159f193cdf903a
MilenaBetsabe <milenabetsabe.e@gmail.com>
2025-03-23 20:47:11 -0300

    Agregar middleware de autenticación utilizando Passport para gestionar la autorización de usuarios

---
+++ src/middlewares/passport.auth.js
@@ -0,0 +1,18 @@
+import passport from 'passport';
+
+const passportAuth = (strategy) => {
+    return async (req, res, next) => {
+        passport.authenticate(strategy, { session: false }, function(error, user, info) {
+            if (error) return next(error);  
+            if (!user) {
+                return res.status(401).send({
+                    error: info && info.message ? info.message : "No autorizado"
+                });
+            }
+            req.user = user;  
+            next();  
+        })(req, res, next);  
+    };
+};
+
+export default passportAuth;
